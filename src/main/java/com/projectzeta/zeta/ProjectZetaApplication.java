2d1fb5f9dc48a20b4b2d0d1549ac5e986c64e330
45257012 <luqh555@gmail.com>
2024-01-04 21:09:13 +0100

    first

---
+++ src/main/java/com/projectzeta/zeta/ProjectZetaApplication.java
@@ -0,0 +1,13 @@
+package com.projectzeta.zeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectZetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectZetaApplication.class, args);
+    }
+
+}
